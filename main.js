17b60eafd9b4efca7d930c6b0ca30180a7d7d407
Angelo Melotti <96485963+callagan70@users.noreply.github.com>
2022-03-09 15:38:28 +0000

    Iniziato Function

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-// let km = prompt ( "quanti km vuoi fare?")
+function interrogatorio(){
+
+let name = prompt ("Come ti chiami?", "The Saint");
+let tessera = prompt ("Sei uno studente o lavori in Boolean? (s/n)");
+let km = prompt ( "Quanti km vuoi fare?");
+let anni = prompt ("In che anno sei nato?")
+
+if (name != null) {
+  document.getElementById("name").innerHTML =  "Hello " + name + "! How are you today?";}
+
+
+}
