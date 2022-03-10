fec0433d53ef66240e849f79f92860667b9ccbd2
Angelo Melotti <96485963+callagan70@users.noreply.github.com>
2022-03-10 00:56:26 +0000

    Sistemati i prezzi a virgola mobile

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ function interrogatorio(){
 
 let name = prompt ("Come ti chiami?", "The Saint");
 let anni = prompt ("In che anno sei nato?", 1970);
-let km = prompt ( "Quanti km vuoi fare?");
+let km = prompt ( "Quanti km vuoi fare?", 123.456);
+
 
 if (name != null) {
   result =`Ciao <i style='color:red; font-size: 32px; font-weight: 700'>${name} !</i> Come te la passi oggi?`;
@@ -12,24 +13,22 @@ if (name != null) {
   result =`La tua età è di <i style="color:red; font-size: 2em">${anni}</i> anni`;
   document.getElementById("eta").innerHTML = result;
   
-  let costo = (km * 0.21)
-  
-  document.getElementById("km").innerHTML = ("Il costo è di " + costo + "€" + " (0,21€ * " + km + "km)")
+  let costo = (km * 0.21).toFixed(2);
+  let ventiPercento =  ((costo / 100) * 80).toFixed(2);
+  let quarantaPercento =  ((costo / 100) * 60).toFixed(2);
 
+  document.getElementById("km").innerHTML = (`Il costo è di <i style="color:red; font-size: 2em">${costo}€</i> (0,21€ * ${km}km)`)
+ 
 if (anni < 18) {
-  document.getElementById("km1").innerHTML = `Ti invidio....Il prezzo a te dedicato è di: ${(costo / 100) * 80} scontato del 20%`;
+  document.getElementById("km1").innerHTML = (`Ti invidio....Il prezzo a te dedicato è di: <i style="color:red; font-size: 2em">${ventiPercento}€</i> scontato del 20%`);
 }
 
 else if (anni > 64){
-document.getElementById("km1").innerHTML = `Sconto dedicato a uno come te che ha lavorato e pagato le tasse tutta la vita è del 40%, indi paghi solo ${(costo / 100) * 60}`;
+document.getElementById("km1").innerHTML = `Sconto dedicato a uno come te che ha lavorato e pagato le tasse tutta la vita è del 40%, indi paghi solo <i style="color:red; font-size: 2em">${quarantaPercento}€</i>`;
 }
 
 else {
-  document.getElementById("km1").innerHTML = `Tu che non sei in pensione e non sei in pensione paghi il prezzo pieno: ${costo}`;
-}
-
-console.log (costo)
-console.log (anni)
+  document.getElementById("km1").innerHTML = `Tu che non sei in pensione paghi il prezzo pieno: <i style="color:red; font-size: 2em">${costo}€</i>`;}
 }
 
 // NOTE come inserire le variabili in un secondo "function" 
